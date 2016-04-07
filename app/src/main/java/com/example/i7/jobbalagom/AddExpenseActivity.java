6e670a3cd78ebadb7bcde87e65133ba7a80a519c
Jakup Güven <jakup2105@gmail.com>
2016-04-07 12:07:37 +0200

    added expense activity

---
+++ app/src/main/java/com/example/i7/jobbalagom/AddExpenseActivity.java
@@ -0,0 +1,13 @@
+package com.example.i7.jobbalagom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
