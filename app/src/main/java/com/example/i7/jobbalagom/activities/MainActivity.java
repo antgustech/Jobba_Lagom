fc74748af47173c9f4fdbe8212c1f5eda632e9dc
Baxtex <baxtex@gmail.com>
2016-04-13 12:26:09 +0200

    ordnade lite med den flytade knappen

--- app/src/main/java/com/example/i7/jobbalagom/activities/MainActivity.java
+++ app/src/main/java/com/example/i7/jobbalagom/activities/MainActivity.java
@@ -95,35 +95,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void floatingButton(){
-        final View actionB = findViewById(R.id.action_b);
-
-        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
-        actionC.setTitle("Hide/Show Action above");
-        actionC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
-            }
-        });
-
-        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
-        menuMultipleActions.addButton(actionC);
-
-
-        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
-        drawable.getPaint().setColor(getResources().getColor(R.color.white));
-
-
-        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
-        actionA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-    }
     //Klick listemer för den flytande knappens alternativ.
     private class ButtonListener implements View.OnClickListener {
         @Override
@@ -143,6 +114,4 @@ public class MainActivity extends AppCompatActivity {
             }
     }
     }
-
-
 }
