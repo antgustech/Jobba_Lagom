3c8d9f09ade9068f393ff7982279e54144d2641c
kajsaorn <kajsaorn@gmail.com>
2016-04-25 11:41:35 +0200

    added setup fragment and setup layout

---
+++ app/src/main/java/com/example/i7/jobbalagom/activities/WorkRegister/SetupFragment.java
@@ -0,0 +1,19 @@
+package com.example.i7.jobbalagom.activities.WorkRegister;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kajsa on 2016-04-25.
+ */
+public class SetupFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(R.layout.setup_fragment, container, savedInstanceState);
+    }
+}
