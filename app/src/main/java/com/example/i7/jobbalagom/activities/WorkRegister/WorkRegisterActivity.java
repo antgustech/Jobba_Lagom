c8322603813e00ab0c24ab276bb0b7aabbd4ba8a
Christoffer Strandberg <Strandberg95@Christoffers-MacBook-Pro.local>
2016-04-25 14:33:06 +0200

    WorkRegister is now functional

--- app/src/main/java/com/example/i7/jobbalagom/activities/WorkRegister/WorkRegisterActivity.java
+++ app/src/main/java/com/example/i7/jobbalagom/activities/WorkRegister/WorkRegisterActivity.java
@@ -4,7 +4,9 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Build;
 import android.os.Bundle;
+
 import android.app.Fragment;
+
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -24,21 +26,35 @@ public class WorkRegisterActivity extends AppCompatActivity {
     private MainTimeRegisterFragment mainTimeRegisterFragment;
     private DateRegisterFragment dateRegisterFragment;
 
-    private MainTimeRegisterCallback mainTimeRegisteCallback;
+    private TimePickerFragment timePickerFromFragment;
+    private TimePickerCallback timePickerFromCallback;
+
+    private TimePickerFragment timePickerToFragment;
+    private TimePickerCallback timePickerToCallback;
+
+    private MainTimeRegisterCallback mainTimeRegisterCallback;
     private DateRegisterCallback dateRegisterCallback;
 
-    private int state = 0;
 
     public WorkRegisterActivity(){
         fragmentManager = getFragmentManager();
+
         mainTimeRegisterFragment = new MainTimeRegisterFragment();
         dateRegisterFragment = new DateRegisterFragment();
+        timePickerFromFragment = new TimePickerFragment();
+        timePickerToFragment = new TimePickerFragment();
 
-        mainTimeRegisteCallback = new MainRegisterListener();
-        mainTimeRegisterFragment.setCallback(mainTimeRegisteCallback);
+        mainTimeRegisterCallback = new MainRegisterListener();
+        mainTimeRegisterFragment.setCallback(mainTimeRegisterCallback);
 
         dateRegisterCallback = new DateRegisterListener();
         dateRegisterFragment.setCallback(dateRegisterCallback);
+
+        timePickerFromCallback = new TimePickerFromListener();
+        timePickerFromFragment.setCallback(timePickerFromCallback);
+
+        timePickerToCallback = new TimePickerToListener();
+        timePickerToFragment.setCallback(timePickerToCallback);
     }
 
     @Override
@@ -48,35 +64,7 @@ public class WorkRegisterActivity extends AppCompatActivity {
 
         setStatusbarColor();
         setupActionBar();
-        setUpStartFragment();
         changeFragment(mainTimeRegisterFragment);
-
-       // dateText_String = "ayo";
-
-
-        //fragmentTransaction.replace()
-
-       // fragmentTransaction.show(dateRegisterFragment);
-
-        //fragmentTransaction.addToBackStack("main");
-
-        //fragmentTransaction.commit();
-
-
-
-
-        //dateText = (EditText) findViewById((R.id.dateText));
-
-        //dateSelecter = new Intent(getApplicationContext(), DateChooser.class);
-        //dateSelecter.putExtra("dateText_String", dateText_String);
-        //startActivity(dateSelecter);
-
-        //dateText.setText(dateText_String);
-      //  new DateUpdater().start();
-        //dateSelecter.onAttachedToWindow();
-    }
-
-    private void setUpStartFragment(){
 
     }
 
@@ -107,17 +95,50 @@ public class WorkRegisterActivity extends AppCompatActivity {
         @Override
         public void buttonPressed(View view) {
             Log.d("ButtonTag", "Ay lmao");
-            changeFragment(dateRegisterFragment);
+
+            if(view.equals(view.findViewById(R.id.dateTextView)))
+                changeFragment(dateRegisterFragment);
+
+            if(view.equals(view.findViewById(R.id.timeTextView_From)))
+                changeFragment(timePickerFromFragment);
+
+            if(view.equals(view.findViewById(R.id.timeTextView_To)))
+                changeFragment(timePickerToFragment);
+
+
         }
     }
 
     private class DateRegisterListener implements DateRegisterCallback{
+
         @Override
         public void UpdateRegisteredDate(int year, int month, int day) {
             //Log.d("DateTag", year + "-" + month + "-" + day);
             changeFragment(mainTimeRegisterFragment);
-            mainTimeRegisterFragment.setTextContainer(year + "-" + month + "-" + day);
+            mainTimeRegisterFragment.setDate_TextContainer(year + "-" + month + "-" + day);
 
+        }
+    }
+
+    private class TimePickerFromListener implements TimePickerCallback{
+
+        @Override
+        public void updateTime(String time) {
+            Log.d("TimeTag", "From" + time);
+
+            changeFragment(mainTimeRegisterFragment);
+            mainTimeRegisterFragment.setTimeFrom_TextContainer(time);
+
+        }
+    }
+    private class TimePickerToListener implements TimePickerCallback{
+
+        @Override
+        public void updateTime(String time) {
+            Log.d("TimeTag","To:" + time);
+
+            changeFragment(mainTimeRegisterFragment);
+            mainTimeRegisterFragment.setTimeTo_TextContainer(time);
 
         }
     }
