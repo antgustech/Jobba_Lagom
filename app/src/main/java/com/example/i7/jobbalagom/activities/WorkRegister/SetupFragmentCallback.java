144385cd0db6e379b329803cb61026332344d2b0
Jakup Güven <jakup2105@gmail.com>
2016-04-27 14:03:15 +0200

    update setupfragment on mainactivity startscreen

---
+++ app/src/main/java/com/example/i7/jobbalagom/activities/WorkRegister/SetupFragmentCallback.java
@@ -0,0 +1,8 @@
+package com.example.i7.jobbalagom.activities.WorkRegister;
+
+/**
+ * Created by Jakup on 2016-04-27.
+ */
+public interface SetupFragmentCallback {
+    public void setupUser(String name, String area, String freeSum, String title, String hWage, String cb);
+}
