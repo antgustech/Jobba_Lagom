8e21badd96620e9b6bc3ef7fc6ebab09c9801ffc
kajsaorn <kajsaorn@gmail.com>
2016-05-04 12:57:10 +0200

    AddJobFragment update

--- app/src/main/java/com/example/i7/jobbalagom/activities/AddJobFragment.java
+++ app/src/main/java/com/example/i7/jobbalagom/activities/AddJobFragment.java
@@ -2,12 +2,16 @@ package com.example.i7.jobbalagom.activities;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.i7.jobbalagom.R;
+import com.example.i7.jobbalagom.activities.callback_interfaces.AddJobFragmentCallback;
 
 /**
  * Created by Kajsa on 2016-05-04.
@@ -15,9 +19,13 @@ import com.example.i7.jobbalagom.R;
 
 public class AddJobFragment extends Fragment {
 
+    private AddJobFragmentCallback callback;
     private EditText inputTitle;
     private EditText inputWage;
-    private EditText inputOB;
+    private TextView tvOBweekday;
+    private TextView tvOBsaturday;
+    private TextView tvOBsunday;
+    private Button btnAdd;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,18 +38,41 @@ public class AddJobFragment extends Fragment {
     }
 
     public void initComponents(View view) {
-
+        inputTitle = (EditText) view.findViewById(R.id.inputTitle);
+        inputWage = (EditText) view.findViewById(R.id.inputWage);
+        tvOBweekday = (TextView) view.findViewById(R.id.tvOBweekday);
+        tvOBsaturday = (TextView) view.findViewById(R.id.tvOBsaturday);
+        tvOBsunday = (TextView) view.findViewById(R.id.tvOBsunday);
+        btnAdd = (Button) view.findViewById(R.id.btnAdd);
+        OBListener obListener = new OBListener();
+        tvOBweekday.setOnClickListener(obListener);
+        tvOBsaturday.setOnClickListener(obListener);
+        tvOBsunday.setOnClickListener(obListener);
+        btnAdd.setOnClickListener(new ButtonListener());
     }
-/**
-    public void setCallBack(LaunchFragmentCallback callback){
+
+    public void setCallBack(AddJobFragmentCallback callback){
         this.callback = callback;
     }
- */
 
     private class ButtonListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+            Log.d("AddJobFragment", "Button pressed");
+        }
+    }
 
+    private class OBListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.tvOBweekday) {
+                Log.d("AddJobFragment", "weekday OB pressed");
+            } else if(v.getId() == R.id.tvOBsaturday) {
+                Log.d("AddJobFragment", "saturday OB pressed");
+            } else if(v.getId() == R.id.tvOBsunday) {
+                Log.d("AddJobFragment", "sunday OB pressed");
+            }
         }
     }
 }
