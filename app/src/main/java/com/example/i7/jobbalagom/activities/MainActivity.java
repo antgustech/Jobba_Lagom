c9206f6bdc6b96ae5eaa5c8e9a95ab3de0d0c0fa
kajsaorn <kajsaorn@gmail.com>
2016-05-09 17:39:31 +0200

    update

--- app/src/main/java/com/example/i7/jobbalagom/activities/MainActivity.java
+++ app/src/main/java/com/example/i7/jobbalagom/activities/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initComponents();
-        //startLaunchFragment();
+        startLaunchFragment();
         setupGraphs();
     }
 
