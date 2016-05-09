284a31e97b7d1be4a9ce27f66921fdf297824c22
kajsaorn <kajsaorn@gmail.com>
2016-05-09 19:53:49 +0200

    add shift fragment input validation

--- app/src/main/java/com/example/i7/jobbalagom/fragments/AddShiftFragment.java
+++ app/src/main/java/com/example/i7/jobbalagom/fragments/AddShiftFragment.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.i7.jobbalagom.R;
 import com.example.i7.jobbalagom.activities.MainActivity;
@@ -48,7 +49,7 @@ public class AddShiftFragment extends Fragment {
         jobTitles = ((MainActivity) getActivity()).getJobTitles();
 
         String log = "Lagrade jobb: ";
-        for(String s : jobTitles) {
+        for (String s : jobTitles) {
             log += s + " ";
         }
         Log.d("AddShiftFragment", log);
@@ -58,18 +59,22 @@ public class AddShiftFragment extends Fragment {
         jobSpinner.setAdapter(adapter);
     }
 
-    public void setCallBack(AddShiftFragmentCallback callback){
+    public void setCallBack(AddShiftFragmentCallback callback) {
         this.callback = callback;
+    }
+
+    public void clearAll() {
+        inputStartTime.setText("");
+        inputEndTime.setText("");
+        inputBreakTime.setText("");
     }
 
     private class ButtonListener implements View.OnClickListener {
 
-
+        String errorMsg;
 
         @Override
         public void onClick(View v) {
-
-
 
             String jobTitle = jobSpinner.getSelectedItem().toString();
             String start = inputStartTime.getText().toString();
@@ -77,12 +82,66 @@ public class AddShiftFragment extends Fragment {
             String breaktime = inputBreakTime.getText().toString();
             int date = 160505;
 
-            float startTime = Float.parseFloat(start.substring(0,2)) + (Float.parseFloat(start.substring(3))/60);
-            float endTime = Float.parseFloat(end.substring(0,2)) + (Float.parseFloat(end.substring(3))/60);
-            float breakHours = Float.parseFloat(breaktime)/60;
+            // Check for invalid input
+
+            errorMsg = "Vänligen ange";
+
+            if (start.equals("") || start.length() != 5 || start.charAt(2) != ':') {
+                addError("starttid i formatet HH:MM");
+            }
+            if (end.equals("") || end.length() != 5 || end.charAt(2) != ':') {
+                addError("sluttid i formatet HH:MM");
+            }
+            if (breaktime.equals("") || breaktime.contains(":")) {
+                addError("rast i minuter");
+            }
+
+            if (!errorMsg.equals("Vänligen ange")) {
+                errorMsg += ".";
+                Toast.makeText(getActivity(), errorMsg, Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            int startHour = Integer.parseInt(start.substring(0, 2));
+            int startMin = Integer.parseInt(start.substring(3));
+            int endHour = Integer.parseInt(end.substring(0, 2));
+            int endMin = Integer.parseInt(end.substring(3));
+
+            if (startHour > 23 || startMin > 59 || endHour > 23 || endMin > 59) {
+                addError("en giltig tid");
+            }
+            if (startHour > endHour || (startHour == endHour && startMin > endMin)) {
+                addError("en sluttid som är senare än starttiden");
+            }
+
+            if (!errorMsg.equals("Vänligen ange")) {
+                errorMsg += ".";
+                Toast.makeText(getActivity(), errorMsg, Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            float startTime = Float.parseFloat(start.substring(0, 2)) + (Float.parseFloat(start.substring(3)) / 60);
+            float endTime = Float.parseFloat(end.substring(0, 2)) + (Float.parseFloat(end.substring(3)) / 60);
+            float breakHours = Float.parseFloat(breaktime) / 60;
             float hoursWorked = endTime - startTime - breakHours;
 
+            if (hoursWorked <= 0) {
+                Toast.makeText(getActivity(), "Tiden du har arbetat är mindre än noll, stämmer verkligen det?", Toast.LENGTH_LONG).show();
+                return;
+            }
+
             callback.addShift(jobTitle, startTime, endTime, hoursWorked, date);
+            clearAll();
+            Toast.makeText(getActivity(), "Arbetspasset har registrerats.", Toast.LENGTH_LONG).show();
+
+        }
+
+        public void addError(String error) {
+            if (errorMsg.charAt(errorMsg.length() - 1) == 'e') {
+                errorMsg = errorMsg + " " + error;
+            } else {
+                errorMsg = errorMsg + ", " + error;
+            }
         }
     }
 }
