c8ee755065e39ecdb28e445e51eae08dfe4bf8ef
baxtex stat <baxtexx@gmail.com>
2016-05-16 21:10:23 +0200

    fixed fab button

---
+++ app/src/test/java/com/example/i7/jobbalagom/test/MainActivityTest.java
@@ -0,0 +1,82 @@
+package com.example.i7.jobbalagom.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Anton Gustafsson on 2016-05-16.
+ * Used for whitebox(branch coverage) testing.
+ */
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testInitComponents() throws Exception {
+
+    }
+
+    @Test
+    public void testOnBackPressed() throws Exception {
+
+    }
+
+    @Test
+    public void testLoadProgressBars() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdatePBcsn() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdatePBincome() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdatePBexpense() throws Exception {
+
+    }
+
+    @Test
+    public void testStartAddExpenseFragment() throws Exception {
+
+    }
+
+    @Test
+    public void testStartAddJobFragment() throws Exception {
+
+    }
+
+    @Test
+    public void testStartSetupFragment() throws Exception {
+
+    }
+
+    @Test
+    public void testStartLaunchFragment() throws Exception {
+
+    }
+
+    @Test
+    public void testStartAddShiftFragment() throws Exception {
+
+    }
+}
