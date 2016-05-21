99898c7e23f84396be602e7455145fe78d6f266c
Strandberg95 <strandberg.coffe@gmail.com>
2016-05-21 23:39:19 +0200

    Apparently i forgot the interface

---
+++ app/src/main/java/com/example/i7/jobbalagom/callback_interfaces/TaxCallbacks/UpdateTaxCallback.java
@@ -0,0 +1,8 @@
+package com.example.i7.jobbalagom.callback_interfaces.TaxCallbacks;
+
+/**
+ * Created by Strandberg95 on 2016-05-20.
+ */
+public interface UpdateTaxCallback {
+    public void UpdateTax(float tax);
+}
