78ae456423aad90f685a8e3a3bb527cce3dfb229
baxtex stat <baxtexx@gmail.com>
2016-05-25 09:40:16 +0200

    Javadoc commented DBHelper.

--- app/src/main/java/com/example/i7/jobbalagom/localDatabase/DBHelper.java
+++ app/src/main/java/com/example/i7/jobbalagom/localDatabase/DBHelper.java
@@ -11,15 +11,12 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
- * Created by Strandberg95 on 2016-05-01.
- * Updated by Anton Gustafsson on 2016-05-02
- * Creates database and it's tables and methods for adding/updating rows and look at them.
+ * Created by Anton, Kajsa, Christoffer, Morgan, Jakup
  */
 
 public class DBHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "INTENAL.DB";
-    private static final int DATABASE_VERSION = 41;
-
+    private static final String DATABASE_NAME = "INTERNAL.DB";
+    private static final int DATABASE_VERSION = 1;
 
     private static final String CREATE_USER_QUERY =
             "CREATE TABLE " + UserContract.User.TABLE_NAME
@@ -64,11 +61,6 @@ public class DBHelper extends SQLiteOpenHelper {
                     + UserContract.OB.OB_INDEX + " FLOAT, "
                     + UserContract.OB.OB_ID + " INTEGER PRIMARY KEY AUTOINCREMENT); ";
 
-    /**
-     * If the Version hasn't changed, it will just open the stored database.
-     * If the version has changed, it will create a new database.
-     */
-
     public DBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         Log.e("DBHelper", "Internal database open");
@@ -95,7 +87,6 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // on upgrade drop older tables
         db.execSQL("DROP TABLE IF EXISTS " + UserContract.User.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + UserContract.Job.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + UserContract.Shift.TABLE_NAME);
@@ -106,7 +97,11 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Adds a user to the user table.
+     * Add a user to db.
+     * @param tax the taxRate aquried from the external database.
+     * @param incomeLimit the incomeLimit choosen by the user.
+     * @param db reference to class that contains db methods.
+     * @param municipality the choosen municipality, also aquried from the eexternal db.
      */
 
     public void addUser(float tax, float incomeLimit, SQLiteDatabase db, String municipality ){
@@ -119,19 +114,31 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Adds a job to job table.
+     * Add a job to db.
+     * @param jobTitle the name of the job.
+     * @param wage the wage choosen by the user.
+     * @param db reference to class that contains db methods.
      */
 
-    public void addJob(String title, float wage, SQLiteDatabase db) {
+    public void addJob(String jobTitle, float wage, SQLiteDatabase db) {
         ContentValues contentValues = new ContentValues();
-        contentValues.put(UserContract.Job.JOB_TITLE, title);
+        contentValues.put(UserContract.Job.JOB_TITLE, jobTitle);
         contentValues.put(UserContract.Job.JOB_WAGE, wage);
         db.insert(UserContract.Job.TABLE_NAME, null, contentValues);
-        Log.e("Internal DB", "New job: " + title + ", timlön: " + wage);
+        Log.e("Internal DB", "New job: " + jobTitle + ", wage: " + wage);
     }
 
     /**
-     * Adds a shift to shift table.
+     * Add a shift to db.
+     * @param jobTitle the name of the job.
+     * @param startTime the starting time for the shift
+     * @param endTime the end time for the shift
+     * @param hoursWorked the startTime-endTime.
+     * @param year the registered year
+     * @param month the registered month
+     * @param day the registered day
+     * @param income the registered complete income for the shift.
+     * @param db reference to class that contains db methods.
      */
 
     public void addShift(String jobTitle, float startTime, float endTime, float hoursWorked, int year, int month, int day, float income, SQLiteDatabase db ){
@@ -149,7 +156,13 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Adds an expense to expense table.
+     * Add expense to db.
+     * @param name the name of the expense.
+     * @param sum the sum of the expense.
+     * @param year the registered year
+     * @param month the registered month
+     * @param day the registered day
+     * @param db reference to class that contains db methods.
      */
 
     public void addExpense(String name, float sum, int year, int month, int day, SQLiteDatabase db ){
@@ -163,6 +176,16 @@ public class DBHelper extends SQLiteOpenHelper {
         Log.e("Internal DB", "New expense: " + year + "" + month + "" + day + ", " + name + ", " + sum + " kr");
     }
 
+    /**
+     * Add ob rate for the registered job.
+     * @param jobTitle the name of the job.
+     * @param day the registered day.
+     * @param fromTime the time the ob rate begins.
+     * @param toTime the time the ob rate ends
+     * @param obIndex the index for the wage is 1 and everything over that is for ob. An Index of 1.5 means that we have 50% of the wage is ob.
+     * @param db reference to class that contains db methods.
+     */
+
     public void addOB(String jobTitle, String day, String fromTime, String toTime, Float obIndex, SQLiteDatabase db) {
         ContentValues contentValues = new ContentValues();
         contentValues.put(UserContract.OB.OB_JOBTITLE, jobTitle);
@@ -174,25 +197,77 @@ public class DBHelper extends SQLiteOpenHelper {
         Log.e("Internal DB", "New OB: " + jobTitle + ", " + (obIndex-1)*100 + " % extra på " + day);
     }
 
-    public void deleteUser(SQLiteDatabase db){
-        db.execSQL("DELETE * FROM " + UserContract.User.TABLE_NAME + "');");
-        Log.e("Internal DB", "Deleted user");
+    /**
+     * Sets the tax rate for the user.
+     * @param currentTax the tax gotten from the external db.
+     * @param db reference to class that contains db methods.
+     */
+
+    public void setTax(float currentTax, SQLiteDatabase db){
+        db.execSQL("UPDATE " + UserContract.User.TABLE_NAME + " SET " + UserContract.User.USER_TAX + "='"+ currentTax + "';");
+        Log.e("Internal DB", "Setting tax in database");
     }
+
+    /**
+     * Sets the income limit for the user.
+     * @param limit the limit choosen by the user.
+     * @param db reference to class that contains db methods.
+     */
+
+    public void setIncomeLimit(float limit, SQLiteDatabase db){
+        db.execSQL("UPDATE " + UserContract.User.TABLE_NAME + " SET " + UserContract.User.USER_INCOME_LIMIT + "='"+ String.valueOf(limit) + "';");
+        Log.e("Internal DB", "Setting income limit in db");
+    }
+
+    /**
+     * Removes the choosen job from the db.
+     * @param jobTitle the name of the job.
+     * @param db reference to class that contains db methods.
+     */
 
     public void deleteJob(String jobTitle, SQLiteDatabase db){
         db.execSQL("DELETE FROM " + UserContract.Job.TABLE_NAME + " WHERE " + UserContract.Job.JOB_TITLE + " = '"+ jobTitle +"';");
         Log.e("Internal DB", "Deleted job: " + jobTitle);
     }
 
+    /**
+     * Removes the user from the db.
+     * @param db reference to class that contains db methods.
+     */
+
+    public void deleteUser(SQLiteDatabase db){
+        db.execSQL("DELETE * FROM " + UserContract.User.TABLE_NAME + "');");
+        Log.e("Internal DB", "Deleted user");
+    }
+
+    /**
+     * Removes shift from the db.
+     * @param id the id of the shift.
+     * @param db reference to class that contains db methods.
+     */
+
     public void deleteShift(int id, SQLiteDatabase db){
         db.execSQL("DELETE FROM " + UserContract.Shift.TABLE_NAME + " WHERE " + UserContract.Shift.SHIFT_ID+ " = '" + id + "';");
         Log.e("Internal DB", "Deleted shift: " + id);
     }
 
+    /**
+     * Removes expense from the db.
+     * @param name the name of the expense.
+     * @param db reference to class that contains db methods.
+     */
+
     public void deleteExpense(String name, SQLiteDatabase db){
         db.execSQL("DELETE FROM " + UserContract.Expense.TABLE_NAME + " WHERE " + UserContract.Expense.EXPENSE_NAME + " = '" + name + "';");
         Log.e("Internal DB", "Deleted expense: " + name);
     }
+
+    /**
+     * Adds all the expenses amounts together used for setting the graphs in mainActivity.
+     * @param db reference to class that contains db methods.
+     * @return the sum of all expense.
+     */
+
 
     public float getTotalExpense(SQLiteDatabase db){
         float totalExpense = 0;
@@ -204,13 +279,16 @@ public class DBHelper extends SQLiteOpenHelper {
         return totalExpense;
     }
 
+    /**
+     * Return either first half or last half of year income-
+     * @param month the registered month.
+     * @param year the registered year.
+     * @param db reference to class that contains db methods.
+     * @return the income of either month 1-6 or 6-12.
+     */
 
     public float getHalfYearIncome(int month, int year, SQLiteDatabase db) {
         float halfYearIncome = 0;
-
-        //int currentYear = 20;
-        //int currentMonth = 7;           // for testing different months
-
         Cursor c;
         if(month <= 6) {
             c = db.rawQuery("SELECT SUM(" + UserContract.Shift.SHIFT_INCOME + ") FROM " + UserContract.Shift.TABLE_NAME
@@ -228,13 +306,19 @@ public class DBHelper extends SQLiteOpenHelper {
         return halfYearIncome;
     }
 
+    /**
+     * Return the income for the choosen month.
+     * @param month the choosen month.
+     * @param year the current year.
+     * @param db reference to class that contains db methods.
+     * @return the income for the choosen month as a float.
+     */
+
     public float getMonthlyIncome(int month, int year, SQLiteDatabase db) {
         float income = 0;
 
         Cursor c = db.rawQuery("SELECT SUM(" + UserContract.Shift.SHIFT_INCOME + ") FROM " + UserContract.Shift.TABLE_NAME + " WHERE "
                 + UserContract.Shift.SHIFT_MONTH + " = " + month + " AND " + UserContract.Shift.SHIFT_YEAR + " = " + year + ";", null);
-
-
         if(c.moveToFirst()) {
             income = c.getFloat(0);
         }
@@ -242,9 +326,16 @@ public class DBHelper extends SQLiteOpenHelper {
         return income;
     }
 
+    /**
+     * Returns the choosen month and year's sum of expenses.
+     * @param month the choosen month.
+     * @param year the choosen year.
+     * @param db reference to class that contains db methods.
+     * @return the sum of expense.
+     */
+
     public float getMonthlyExpenses(int month, int year, SQLiteDatabase db) {
         float expenses = 0;
-
         Cursor c = db.rawQuery("SELECT SUM(" + UserContract.Expense.EXPENSE_AMOUNT + ") FROM " + UserContract.Expense.TABLE_NAME + " WHERE "
                 + UserContract.Expense.EXPENSE_MONTH + " = " + month + " AND " + UserContract.Expense.EXPENSE_YEAR + " = " + year + ";", null);
 
@@ -255,21 +346,16 @@ public class DBHelper extends SQLiteOpenHelper {
         return expenses;
     }
 
-
-
-
-
-
-
-
-
-    // TA BORT??
+    /**
+     * Gets the current month income
+     * @param db reference to class that contains db methods.
+     * @return this month incsome as a float.
+     */
 
     public float getThisMonthsIncome(SQLiteDatabase db) {
         float thisMonthsIncome = 0;
         int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
         int currentYear = Calendar.getInstance().get(Calendar.YEAR)%100;
-        //int currentMonth = 7;                  // for testing different months
 
         Cursor c = db.rawQuery("SELECT SUM(" + UserContract.Shift.SHIFT_INCOME + ") FROM " + UserContract.Shift.TABLE_NAME
                                 + " WHERE " + UserContract.Shift.SHIFT_MONTH + " = " + currentMonth
@@ -282,11 +368,16 @@ public class DBHelper extends SQLiteOpenHelper {
         return thisMonthsIncome;
     }
 
+    /**
+     * Gets this month expense.
+     * @param db reference to class that contains db methods.
+     * @return this months expense as a float.
+     */
+
     public float getThisMonthsExpenses(SQLiteDatabase db) {
         float thisMonthsExpenses = 0;
         int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
         int currentYear = Calendar.getInstance().get(Calendar.YEAR)%100;
-        //int currentMonth = 1;                  // for testing different months
 
         Cursor c = db.rawQuery("SELECT SUM(" + UserContract.Expense.EXPENSE_AMOUNT + ") FROM " + UserContract.Expense.TABLE_NAME
                 + " WHERE " + UserContract.Expense.EXPENSE_MONTH + " = " + currentMonth
@@ -298,6 +389,13 @@ public class DBHelper extends SQLiteOpenHelper {
         return thisMonthsExpenses;
     }
 
+
+    /**
+     * Gets the incomeLimit set by the user
+     * @param db reference to class that contains db methods.
+     * @return the set incomeLimit as a float.
+     */
+
     public float getIncomeLimit(SQLiteDatabase db) {
         float incomeLimit = 0;
         Cursor c = db.rawQuery("SELECT " + UserContract.User.USER_INCOME_LIMIT + " FROM " + UserContract.User.TABLE_NAME + ";", null);
@@ -307,6 +405,12 @@ public class DBHelper extends SQLiteOpenHelper {
         Log.e("Internal DB", "Getting income limit sum from database " + incomeLimit );
         return incomeLimit;
     }
+
+    /**
+     * Gets all the job names for setting the spinner so the user can se their registered jobs.
+     * @param db reference to class that contains db methods.
+     * @return string array with all the job names.
+     */
 
     public String[] getJobTitles(SQLiteDatabase db){
         ArrayList<String> jobs = new ArrayList<String>();
@@ -320,6 +424,13 @@ public class DBHelper extends SQLiteOpenHelper {
         return jobTitles;
     }
 
+    /**
+     * Gets the wage for the choosen job.
+     * @param jobTitle the name of the job
+     * @param db reference to class that contains db methods.
+     * @return the wage as a float for the choosen job.
+     */
+
     public float getWage(String jobTitle, SQLiteDatabase db) {
         float wage = 0;
         Cursor c = db.rawQuery("SELECT " + UserContract.Job.JOB_WAGE + " FROM " + UserContract.Job.TABLE_NAME
@@ -331,6 +442,8 @@ public class DBHelper extends SQLiteOpenHelper {
         return wage;
     }
 
+
+    //TODO:REMOVE these two methods?
     public float getStartTime(SQLiteDatabase db) {
         float sum = 0;
         Cursor c = db.rawQuery("SELECT " + UserContract.Shift.SHIFT_START_TIME + " FROM " + UserContract.Shift.TABLE_NAME + ";", null);
@@ -351,6 +464,13 @@ public class DBHelper extends SQLiteOpenHelper {
         return sum;
     }
 
+
+    /**
+     * Gets the stored tax in the db.
+     * @param db reference to class that contains db methods.
+     * @return the set tax as a float.
+     */
+
     public float getUserTax(SQLiteDatabase db) {
         float sum = 0;
         Cursor c = db.rawQuery("SELECT " + UserContract.User.USER_TAX + " FROM " + UserContract.User.TABLE_NAME + ";", null);
@@ -362,6 +482,12 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
+    /**
+     * Gets the registered municipality
+     * @param db reference to class that contains db methods.
+     * @return the set municipality as a string.
+     */
+
     public String getUserMunicipality(SQLiteDatabase db){
         String municipality = "";
         Cursor c = db.rawQuery("SELECT " + UserContract.User.USER_MUNICIPALITY + " FROM " + UserContract.User.TABLE_NAME + ";", null);
@@ -371,6 +497,14 @@ public class DBHelper extends SQLiteOpenHelper {
 
         return municipality;
     }
+
+    /**
+     *  Gets the ob rate for the choosen job name and day.
+     * @param jobTitle the name of the job.
+     * @param day the day the ob was set to.
+     * @param db reference to class that contains db methods.
+     * @return the ob rate as a float.
+     */
 
     public float getOB(String jobTitle, String day, SQLiteDatabase db){
         float sum = 0;
@@ -382,9 +516,17 @@ public class DBHelper extends SQLiteOpenHelper {
         return sum;
     }
 
-    public float getOBStart(String jobTitle, String obDay, SQLiteDatabase db){
+    /**
+     * Gets the start time of the ob rate for the choosen job and day.
+     * @param jobTitle the name of the job.
+     * @param day the day the ob was set to.
+     * @param db reference to class that contains db methods.
+     * @return the time the ob rate begins as a float.
+     */
+
+    public float getOBStart(String jobTitle, String day, SQLiteDatabase db){
         float sum = 0;
-        Cursor c = db.rawQuery("SELECT " + UserContract.OB.OB_FROMTIME + " FROM " + UserContract.OB.TABLE_NAME + " WHERE jobTitle ='" + jobTitle +"' AND day='"+obDay+"';", null);
+        Cursor c = db.rawQuery("SELECT " + UserContract.OB.OB_FROMTIME + " FROM " + UserContract.OB.TABLE_NAME + " WHERE jobTitle ='" + jobTitle +"' AND day='"+day+"';", null);
         if (c.moveToFirst()) {
             sum = c.getFloat(c.getColumnIndex(UserContract.OB.OB_FROMTIME));
         }
@@ -392,9 +534,17 @@ public class DBHelper extends SQLiteOpenHelper {
         return sum;
     }
 
-    public float getOBEnd(String jobTitle, String obDay, SQLiteDatabase db){
+    /**
+     * Gets the end time of the ob rate for the choosen job and day.
+     * @param jobTitle the name of the job.
+     * @param day the day the ob was set to.
+     * @param db reference to class that contains db methods.
+     * @return the time the ob rate ends as a float.
+     */
+
+    public float getOBEnd(String jobTitle, String day, SQLiteDatabase db){
         float sum = 0;
-        Cursor c = db.rawQuery("SELECT " + UserContract.OB.OB_TOTIME + " FROM " + UserContract.OB.TABLE_NAME + " WHERE jobTitle ='" + jobTitle+"' AND day='"+obDay+"';", null);
+        Cursor c = db.rawQuery("SELECT " + UserContract.OB.OB_TOTIME + " FROM " + UserContract.OB.TABLE_NAME + " WHERE jobTitle ='" + jobTitle+"' AND day='"+day+"';", null);
         if (c.moveToFirst()) {
             sum = c.getFloat(c.getColumnIndex(UserContract.OB.OB_TOTIME));
         }
@@ -402,50 +552,16 @@ public class DBHelper extends SQLiteOpenHelper {
         return sum;
     }
 
+    /**
+     * Checks whenever a user has been created or not.
+     * @param db reference to class that contains db methods.
+     * @return true if there already is a user created.
+     */
+
     public boolean isUserCreated(SQLiteDatabase db) {
         Cursor c = db.rawQuery("SELECT * FROM " + UserContract.User.TABLE_NAME + ";", null);
-
-        boolean isCreated;
         if(c.getCount() == 0) {
             return false;
         } return true;
     }
-
-    /**
-     * This will set all rows to the specified tax. But as we are only going to have 1 user at the time, this is not a problem.
-     * @param currentTax
-     * @param db
-     */
-
-    public void setTax(float currentTax, SQLiteDatabase db){
-        db.execSQL("UPDATE " + UserContract.User.TABLE_NAME + " SET " + UserContract.User.USER_TAX + "='"+ currentTax + "';");
-        Log.e("Internal DB", "Setting tax in database");
-    }
-
-
-
-
-    /*public ArrayList<Float> getTotalExpense(SQLiteDatabase db){
-        ArrayList<Float> list = new ArrayList<Float>();
-        Cursor c = db.rawQuery("select sum from " + UserContract.Expense.TABLE_NAME , null);
-        while(c.moveToNext()){
-            float sum = c.getFloat(c.getColumnIndex("sum"));
-            list.add(sum);
-        }
-        Log.e("DBTAG", list.toString());
-        return list;
-    }
-    */
-    public void setIncomeLimit(float limit, SQLiteDatabase db){
-        db.execSQL("UPDATE " + UserContract.User.TABLE_NAME + " SET " + UserContract.User.USER_INCOME_LIMIT + "='"+ String.valueOf(limit) + "';");
-        Log.e("Internal DB", "Setting income limit in db");
-    }
-
-
-
-
-
-
-
-
 }
